8054250b54aa5bdb2282e08596884ce7f0c19ec5
Celiacaoyanan <celiacaoyanan@outlook.com>
2019-02-13 23:45:42 -0500

    Create 28 Implement strStr().java

---
+++ two pointers/28 Implement strStr().java
@@ -0,0 +1,13 @@
+//haystack’s length may be smaller than needle
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for (int i=0;i<haystack.length()-needle.length()+1;i++) { // cant use i<haystack.length(), because aaa, aaaa
+            int j=0;
+            for (j=0;j<needle.length();j++) {
+                if (haystack.charAt(i+j)!=needle.charAt(j)) break;
+            }
+            if (j==needle.length()) return i;
+        }
+        return -1;
+    }
+}
