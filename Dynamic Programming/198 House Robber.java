d69d8df5c30736a5df055722a23541e51f10155e
Celiacaoyanan <celiacaoyanan@outlook.com>
2019-02-16 21:39:47 -0500

    Create 198 House Robber.java

---
+++ Dynamic Programming/198 House Robber.java
@@ -0,0 +1,13 @@
+//f[i] record the max money we can get from robbing the first i houses
+class Solution {
+    public int rob(int[] nums) {
+        if (nums==null || nums.length==0) return 0;
+        int[] f=new int[nums.length+1];
+        f[0]=0;
+        f[1]=nums[0];
+        for (int i=2;i<=nums.length;i++) {
+            f[i]=Math.max(f[i-1],f[i-2]+nums[i-1]);
+        }
+        return f[nums.length];
+    }
+}
